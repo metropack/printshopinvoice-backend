dfd1e95a772ab23c3f522262e168d741d610ecee
Metro Print <mpsinc02@gmail.com>
2025-09-08 17:22:06 -0400

    CORS: allow X-Requested-With (fix preflight from localhost:3000)

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  
 };
 
 app.use(cors(corsOptions));
